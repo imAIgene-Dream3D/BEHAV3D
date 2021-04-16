505a823aecea57c73e77ab2b140edee0416608b5
alievakrash <68925157+alievakrash@users.noreply.github.com>
2021-04-16 17:02:59 +0200

    Add files via upload

---
+++ scripts/Organoid death dynamics/Death dynamics per well/Calculate_mean_dead_dye_perwell.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(scales)
+library(MESS)
+##Import dataframe live_deadROI7 for each experiment. Combine several if necessary
+Combi<-readRDS("D:/R/scripts/T_cell paper/FINAL SCRIPTS_20210408/Fig1/example_data_set_organoids/live_deadROI7_example_data")
+
+
+## Rescale the mean dead cell dye per experiment
+Combi<-Combi%>% 
+  group_by(exp) %>% 
+  mutate(red_sc=rescale(red, to=c(0,100)))%>%ungroup()
+## for each well normalize the dead cell dye mean intensity to timepoint 1
+Combi<-Combi%>%group_by(Org, type, exp)%>%arrange(Time2)%>%mutate(red_norm=red_sc - first(red_sc))%>%filter(Time2<23)
+
+## calculate area under the curve for each donor/T cell co-culture
+Area_under_Curve<-Combi%>%
+  group_by(Org, type, exp)%>%summarise(auc=auc(Time2,red_norm))%>%ungroup()
+## calculate delta increase for each donor/T cell co-culture
+delta <-Combi%>%group_by(Org, type, exp)%>%arrange(Time2)%>%summarize(delta= last(red_norm) - first(red_norm))%>%ungroup()
+## combine both delta and AUC in one dataframe
+AUC_delta<-left_join(Area_under_Curve,delta,by=c("Org","type", "exp"))
+library(xlsx)
+## export calculation for plot 
+write.xlsx(AUC_delta, file = "AUC_delta")
