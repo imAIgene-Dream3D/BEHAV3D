e02bd062759b369a065a065a6b70a0b632817e5b
Sam de Blank <SamdeBlank@hotmail.com>
2023-11-29 15:31:48 +0100

    Remove and gitignore build directory

--- build/lib/behav3d/__init__.py
+++
@@ -1,9 +0,0 @@
-def format_time(
-    start_time,
-    end_time
-):
-    elapsed_time = end_time - start_time
-    hours = int(elapsed_time // 3600)
-    minutes = int((elapsed_time % 3600) // 60)
-    seconds = int(elapsed_time % 60)
-    return(hours, minutes, seconds)
