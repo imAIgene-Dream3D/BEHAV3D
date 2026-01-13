5eaf9bb35d20ed9eb41362cc67d858ab0ec88a65
Sam de Blank <41326187+SdeBlank@users.noreply.github.com>
2026-01-13 18:17:11 +0100

    Few restructure extra changes

--- behav3d/__init__.py
+++ behav3d/__init__.py
@@ -1,51 +1,51 @@
-# Public API for BEHAV3D
+# # Public API for BEHAV3D
 
-# Core utilities
-from .core.metadata import (
-    load_behav3d_metadata,
-    check_behav3d_metadata,
-    detect_organoid_types_from_metadata,
-    detect_immune_cell_types_from_metadata,
-    detect_other_cell_types_from_metadata,
-    has_dead_channel,
-    has_dead_mask
-)
-from .core.utils import (
-    format_time,
-    convert_time,
-    get_current_time,
-    convert_distance,
-    element_to_dict,
-    rel_elsize,
-    expand_column_patterns
-)
+# # Core utilities
+# from .core.metadata import (
+#     load_behav3d_metadata,
+#     check_behav3d_metadata,
+#     detect_organoid_types_from_metadata,
+#     detect_immune_cell_types_from_metadata,
+#     detect_other_cell_types_from_metadata,
+#     has_dead_channel,
+#     has_dead_mask
+# )
+# from .core.utils import (
+#     format_time,
+#     convert_time,
+#     get_current_time,
+#     convert_distance,
+#     element_to_dict,
+#     rel_elsize,
+#     expand_column_patterns
+# )
 
-# Preprocessing / Filtering
-from .preprocessing.filtering import (
-    filter_minimal_track_length,
-    filter_by_full_duration,
-    trim_to_maximal_track_length,
-    plot_filter_count
-)
+# # Preprocessing / Filtering
+# from .preprocessing.filtering import (
+#     filter_minimal_track_length,
+#     filter_by_full_duration,
+#     trim_to_maximal_track_length,
+#     plot_filter_count
+# )
 
-# IO
-from .io.images import (
-    load_image,
-    load_image_metadata,
-    get_filepath_stem,
-    convert_axis_order,
-    convert_file_to_zarr,
-    convert_input_files_to_zarr,
-    load_elsizes,
-    get_image_shape,
-    get_image_dimension_order,
-    load_zarr,
-    save_as_zarr,
-    append_to_zarr
-)
+# # IO
+# from .io.images import (
+#     load_image,
+#     load_image_metadata,
+#     get_filepath_stem,
+#     convert_axis_order,
+#     convert_file_to_zarr,
+#     convert_input_files_to_zarr,
+#     load_elsizes,
+#     get_image_shape,
+#     get_image_dimension_order,
+#     load_zarr,
+#     save_as_zarr,
+#     append_to_zarr
+# )
 
-# Re-export widgets package for convenience
-try:
-    from . import widgets
-except ImportError:
-    pass
+# # Re-export widgets package for convenience
+# try:
+#     from . import widgets
+# except ImportError:
+#     pass
