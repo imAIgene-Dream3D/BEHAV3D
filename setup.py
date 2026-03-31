eb969642666a38c317c7a838d7e3a513ddbef2d6
Erios12 <eriosjimenez00@gmail.com>
2026-03-31 11:31:26 +0200

    Revert installation and setup files to install the napari plugin, lost during merging with dev branch

--- setup.py
+++ setup.py
@@ -9,5 +9,21 @@ setup(
     author_email='S.deBlank-3@prinsesmaximacentrum.nl',
     license='Apache License 2.0',
     packages=setuptools.find_packages(),
-    install_requires=[]
+    package_data={
+        "behav3d.napari": ["napari.yaml"],
+    },
+    install_requires=[
+        "napari>=0.5.0",
+        "magicgui>=0.8",
+        "qtpy>=2.4",
+        "dask",
+        "zarr",
+        "pandas",
+        "pyyaml",
+    ],
+    entry_points={
+        "napari.manifest": [
+            "behav3d = behav3d.napari:napari.yaml",
+        ],
+    },
 )
